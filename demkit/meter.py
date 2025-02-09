5af1d2d2235b8cc54383d8015b0160a06a811fd7
Samuel Bruin <S.bruin-1@student.tudelft.nl>
2025-02-09 13:50:24 +0000

    Add docs for meter api functions

--- demkit/meter.py
+++ demkit/meter.py
@@ -1,12 +1,22 @@
-from const import BASE_URL  # noqa: D100
-from model.measurement import Measurement
-import requests
+from model.measurement import Measurement  # noqa: D100
 from rest import _get_measurement
 
 
 def get_import() -> Measurement:
+    """Retrieve the import measurement from the smart meter.
+
+    Returns:
+        Measurement: The import measurement object retrieved from the smart meter
+
+    """
     return _get_measurement("/houses/0/meters/0/import")
 
 
 def get_export() -> Measurement:
-    return _get_measurement("/houses/0/meters/0/import")
+    """Retrieve the export measurement from the smart meter.
+
+    Returns:
+        Measurement: The export measurement object retrieved from the smart meter
+
+    """
+    return _get_measurement("/houses/0/meters/0/export")

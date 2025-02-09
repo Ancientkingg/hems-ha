af639956a4fad9ee5304263f0f3b5b62bb29130f
Samuel Bruin <S.bruin-1@student.tudelft.nl>
2025-02-09 13:56:27 +0000

    Add battery model

---
+++ demkit/model/battery_info.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass  # noqa: D100
+from enum import Enum
+
+
+class BatteryStatus(Enum):
+    """Enum representing the status of a battery.
+
+    Attributes:
+        CHARGING (str): The battery is currently charging.
+        DISCHARGING (str): The battery is currently discharging.
+        IDLE (str): The battery is idle and not charging or discharging.
+        ERROR (str): There is an error with the battery.
+
+    """
+
+    CHARGING = "CHARGING"
+    DISCHARGING = "DISCHARGING"
+    IDLE = "IDLE"
+    ERROR = "ERROR"
+
+
+@dataclass
+class BatteryInfo:
+    """A class to represent the information of a battery.
+
+    Attributes:
+        capacity (float): The total capacity of the battery in Wh.
+        max_charge (float): The maximum charge rate of the battery in W.
+        max_discharge (float): The maximum discharge rate of the battery in W.
+        state_of_charge (float): The current state of charge of the battery as a percentage (0-100%).
+        status (BatteryStatus): The current status of the battery.
+        consumption (float): The current consumption of the battery in W.
+
+    """
+
+    max_charge: float
+    max_discharge: float
+    state_of_charge: float
+    status: BatteryStatus
+    consumption: float
